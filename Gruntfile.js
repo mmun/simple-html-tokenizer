3a15ab34f47673e9371ca96fb2b6a0adefad7e9e
Martin Muñoz <im.mmun@gmail.com>
2014-04-02 01:24:42 -0400

    Use Broccoli, ES6 and Travis

---
+++ Gruntfile.js
@@ -0,0 +1,101 @@
+module.exports = function(grunt) {
+  grunt.registerTask('dev', function() {
+    require('./bower_components/qunit/qunit/qunit.js');
+    // console.log(QUnit.module);
+    // console.log(process.cwd());
+    // module = {hi: 0};
+    // console.log(module);
+    console.log(QUnit.test === test);
+  });
+};
+// module.exports = function(grunt) {
+//   grunt.registerTask('dev', [
+//     'dev-build',
+//     'connect:test',
+//     'watch:dev-build'
+//   ]);
+
+//   grunt.registerTask('dev-build', [
+//     'clean',
+//     'jshint', 
+//     'transpile:lib', 'transpile:test', 
+//     'concat:lib', 'concat:test'
+//   ]);
+
+//   grunt.loadNpmTasks('grunt-contrib-clean');
+//   grunt.loadNpmTasks('grunt-contrib-concat');
+//   grunt.loadNpmTasks('grunt-contrib-connect');
+//   grunt.loadNpmTasks('grunt-contrib-jshint');
+//   grunt.loadNpmTasks('grunt-contrib-watch');
+//   grunt.loadNpmTasks('grunt-es6-module-transpiler');
+
+//   grunt.initConfig({
+//     pkg: grunt.file.readJSON('package.json'),
+
+//     clean: ['tmp'],
+
+//     concat: {
+//       'lib': {
+//         src: 'tmp/transpiled/lib/**/*.js',
+//         dest: 'tmp/assets/lib.js'
+//       },
+//       'test': {
+//         src: 'tmp/transpiled/test/**/*.js',
+//         dest: 'tmp/assets/test.js'
+//       }
+//     },
+
+//     connect: {
+//       'test': {
+//         options: {
+//           base: ['bower_components', 'tmp', 'test-env']
+//         }
+//       }
+//     },
+
+//     watch: {
+//       'dev-build': {
+//         files: ['lib/**', 'test/**', 'test-env/**'],
+//         tasks: ['dev-build'],
+//         options: {
+//           livereload: true
+//         }
+//       },
+//     },
+
+//     jshint: {
+//       options: {
+//         'jshintrc': '.jshintrc'
+//       },
+//       all: ['lib/**/*.js', 'test/**/*.js']
+//     },
+
+//     transpile: {
+//       'lib': {
+//         moduleName: function(path) {
+//           return grunt.config.process('<%= pkg.name %>') + path;
+//         },
+//         type: 'amd',
+//         files: [{
+//           expand: true,
+//           cwd: 'lib',
+//           src: '**/*.js',
+//           dest: 'tmp/transpiled/lib'
+//         }]
+//       },
+
+//       'test': {
+//         moduleName: function(path) {
+//           return grunt.config.process('<%= pkg.name %>/test') + path;
+//         },
+//         type: 'amd',
+//         files: [{
+//           expand: true,
+//           cwd: 'test',
+//           src: '**/*.js',
+//           dest: 'tmp/transpiled/test'
+//         }]
+//       }
+//     }
+//   });
+// };
